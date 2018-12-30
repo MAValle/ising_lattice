39b96519c5f46b67effdf37d61e78f7fb93ca5cd
MAValle <mauvalle@gmail.com>
2018-12-30 15:30:56 -0300

    Creation of the clon at 301218

---
+++ lattice_v1.R
@@ -0,0 +1,9 @@
+# Este script es un primer intento para genera runa grilla que representa
+# un lattice Ising de N X N .
+
+#File name: lattice_v1.R
+#Creation date: 30-dic-18
+
+# Notas:
+# 30-dic-18: cration
+#
